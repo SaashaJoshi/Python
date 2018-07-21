e6990d45804a1ee7b60076f2e3e63c964d5d8163
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 11:46:39 +0530

    Update alternatingCaps.py

--- Methods and Functions/alternatingCaps.py
+++ Methods and Functions/alternatingCaps.py
@@ -1,1 +1,8 @@
-
+def altCaps(string):
+  newString=''
+  for item, index in enumerate(string):
+    if index%2==0:
+      newString+=item.upper()
+    else:
+      newString+=item.lower()
+  print(newString)
