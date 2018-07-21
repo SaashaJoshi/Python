ae6d49104843d1aff9ed991a5be3a1f4d7fcd13e
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 11:50:26 +0530

    Update alternatingCaps.py

--- Methods and Functions/alternatingCaps.py
+++ Methods and Functions/alternatingCaps.py
@@ -1,8 +1,8 @@
 def altCaps(string):
   newString=''
-  for item, index in enumerate(string):
+  for item, index in enumerate(string):   #uses enumerate function to get the item and its index in the string in the form of a tuple.
     if index%2==0:
-      newString+=item.upper()
+      newString+=item.upper()   #uppercases the item at even index
     else:
-      newString+=item.lower()
+      newString+=item.lower()   #lowercases the item at odd index
   print(newString)
