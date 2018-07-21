9bb554b0fa3d0661ac88b7585917482d4ca4aa57
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 11:55:49 +0530

    Update alliteration.py

--- Methods and Functions/alliteration.py
+++ Methods and Functions/alliteration.py
@@ -1,2 +1,7 @@
-def allit():
+def allit(text):
+  list=text.split()     #converts string to a list  #can even use text.lower().split()
+  if list[0][0].lower()==list[1][0].lower():      #compares the first letter of the different items in the list created
+    return True
+  else:
+    return False
   
