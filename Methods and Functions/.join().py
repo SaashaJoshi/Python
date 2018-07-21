5781abc0869c3ea6a46e8b3e3bfa6b08ddbacb81
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 12:05:59 +0530

    Update .join().py

--- Methods and Functions/.join().py
+++ Methods and Functions/.join().py
@@ -1,1 +1,8 @@
+string1='123'
+string2='abc'
 
+#concatenates characters of string 2 in front of string 1
+print('string1.join(string2): ', string1.join(string2))
+
+#concatenates items of string 1 in front of string 2
+print('string2.join(string1): ', string2.join(string1))
