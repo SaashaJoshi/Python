30346013cc9af848a2614c1d315437da1a645b02
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 12:27:07 +0530

    Update vowels.py

--- Methods and Functions/vowels.py
+++ Methods and Functions/vowels.py
@@ -1,1 +1,7 @@
-
+def vowels(text):
+  order=['a','e', 'i', 'o', 'u', 'x']     #checks if aeiou occur in the given order in the string
+  for char in text:
+    if char==order[0]:
+      order.pop(0)      #or use order.remove(char)
+  if len(order)==1:   #is true when all the vowels are found in the given order in the string
+    print('Yes, vowels occur in the given order!')
