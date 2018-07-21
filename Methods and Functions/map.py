4626cfef5ce4741fa3d327a018d5dcdbf7346734
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 20:03:07 +0530

    Update map.py

--- Methods and Functions/map.py
+++ Methods and Functions/map.py
@@ -5,3 +5,6 @@ myList=[1,2,3,4,5]
 
 for item in map(square, myList):    #map function applies the function (mentioned as the first argument) to the iterable (mentioned as the second argument)
   print(item)
+  
+#or
+print(list(map(square, myList)))
