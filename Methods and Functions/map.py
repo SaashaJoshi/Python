1af3e9e7fb5c1910cbb739e9e9e57b5cf249774f
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-21 20:00:11 +0530

    Update map.py

--- Methods and Functions/map.py
+++ Methods and Functions/map.py
@@ -1,1 +1,7 @@
+def square(num):
+  return num**2
 
+myList=[1,2,3,4,5]
+
+for item in map(square, myList):    #map function applies the function (mentioned as the first argument) to the iterable (mentioned as the second argument)
+  print(item)
