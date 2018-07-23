2dfdd15f4aa8006e083a6ab49c6d4555558cf3dd
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-23 22:38:09 +0530

    Update linearSearch.py

--- Methods and Functions/linearSearch.py
+++ Methods and Functions/linearSearch.py
@@ -2,7 +2,6 @@ numlist=input('Enter the list: ')
 numlist=numlist.split(',')
 target=input('Enter the element to be searched: ')
 for position, element in enumerate(numlist):
-    print(position, element)
     if element==target:
         print(f'Element found at position : {position}')
         break
