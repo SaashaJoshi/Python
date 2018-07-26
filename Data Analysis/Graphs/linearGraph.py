f668b003ac036ccc539cb09082f4e997effe5132
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-07-26 20:46:16 +0530

    Update linearGraph.py

--- Data Analysis/Graphs/linearGraph.py
+++ Data Analysis/Graphs/linearGraph.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+x = [1, 2, 3]
+y = [1, 4, 9]
+z = [10, 0, 5]
+plt.plot(x, y)
+plt.plot(x, z)
+
+plt.title('Test Plot')
+
+plt.xlabel('X')
+plt.ylabel('Y and Z')
+
+plt.legend(['This is Y', 'This is Z'])
+plt.show()
