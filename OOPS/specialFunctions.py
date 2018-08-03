f03ef0ab28b7171a4d1c9348c242b63f462af8ca
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-03 16:31:50 +0530

    Update specialFunctions.py

--- OOPS/specialFunctions.py
+++ OOPS/specialFunctions.py
@@ -4,10 +4,10 @@ class Book():
     self.author=author
     self.pages=pages
     
-  def __str__(self):      # special method string
+  def __str__(self):      # special method string     # str return string representations of objects
     return f'The author of {self.name} is {self.author}'
   
-  def __len__(self):      # special method length
+  def __len__(self):      # special method length     # len returns the length of the object
     return self.pages
   
   def __del__(self):    # special method delete
