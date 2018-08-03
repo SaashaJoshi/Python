52ad35b316f5e1d108c8a01233ca7be040be2edc
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-03 16:30:04 +0530

    Update specialFunctions.py

--- OOPS/specialFunctions.py
+++ OOPS/specialFunctions.py
@@ -1,1 +1,14 @@
-
+class Book():
+  def __init__(self, name, author, pages):
+    self.name=name
+    self.author=author
+    self.pages=pages
+    
+  def __str__(self):      # special method string
+    return f'The author of {self.name} is {self.author}'
+  
+  def __len__(self):      # special method length
+    return self.pages
+  
+  def __del__(self):    # special method delete
+    print('A book has been deleted!')
