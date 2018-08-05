705b147baf84687957cac889c83e6af283c130b8
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-05 23:31:18 +0530

    Update primeNotPrime.py

--- Methods and Functions/primeNotPrime.py
+++ Methods and Functions/primeNotPrime.py
@@ -1,3 +1,17 @@
 # This function returns a list of all those numbers in the interval [a, b) 
-whose digits are made up of prime numbers (2, 3, 5, 7) but which are not primes themselves.
+#  whose digits are made up of prime numbers (2, 3, 5, 7) but which are not primes themselves.
 
+from math import sqrt
+
+def not_primes(a, b):
+    final=[]
+    for i in range(a,b):
+        for j in range(2, int(sqrt(i)) + 1):
+            if i %j  == 0:
+                for x in str(i):
+                    if x not in '2357':
+                        break
+                else:
+                    final.append(i)
+                break
+    return final
