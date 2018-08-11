31277670b7d9d8374807e23bfc8ec316adcc4ddc
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2018-08-11 23:36:40 +0530

    Update error.py

--- Errors and Exceptions/error.py
+++ Errors and Exceptions/error.py
@@ -1,1 +1,12 @@
-
+def func():
+  while True:
+    try:
+      numb=int(input('Please enter a number: '))
+    except:
+      print('An error occured!')
+      continue
+    else:
+      break
+    
+  print(f'The square of number is: {numb**2}')
+  
