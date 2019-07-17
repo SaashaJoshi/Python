d02ffdcf6e446e09359a00b4d5d1c045ce3b88a7
Saasha Joshi <32019413+SaashaJoshi@users.noreply.github.com>
2019-07-17 20:46:47 +0530

    Update linearGraph.py

--- Matplotlib/linearGraph.py
+++ Matplotlib/linearGraph.py
@@ -1,15 +1,16 @@
+# Using Matplotlib to plot a Linear Graph
 import matplotlib.pyplot as plt
-
 x = [1, 2, 3]
 y = [1, 4, 9]
 z = [10, 0, 5]
-plt.plot(x, y)
-plt.plot(x, z)
 
+plt.plot(x, y, label='This is Y')
+plt.plot(x, z, label='This is Z')
 plt.title('Test Plot')
-
 plt.xlabel('X')
 plt.ylabel('Y and Z')
 
-plt.legend(['This is Y', 'This is Z'])
+# Using args in plt.legend without arg label in plt.plot (Error prone!)
+# plt.legend(['This is Y', 'This is Z'])
+
 plt.show()
